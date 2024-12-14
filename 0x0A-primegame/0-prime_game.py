66fe95585aab70926633faacc999b1e1ae03fa8e
Nosipho2021 <lorrainemdk@gmail.com>
2024-12-14 13:55:58 -0800

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
 
-
-def primes(n):
-    """Return list of prime numbers between 1 and n inclusive
+def sieve_of_eratosthenes(limit):
     """
-    prime = []
-    sieve = [True] * (n + 1)
-    for p in range(2, n + 1):
-        if (sieve[p]):
-            prime.append(p)
-            for i in range(p, n + 1, p):
-                sieve[i] = False
-    return prime
+    Generate a list of prime numbers up to the given limit
+    using the Sieve of Eratosthenes.
+    """
+    primes = [True] * (limit + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, limit + 1, i):
+                primes[j] = False
+    return primes
 
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game
+    Determine the winner of the prime game played
+    for x rounds with given nums.
     """
-    if x is None or nums is None or x == 0 or nums == []:
+    if not nums or x < 1:
         return None
-    Maria = Ben = 0
-    for i in range(x):
-        prime = primes(nums[i])
-        if len(prime) % 2 == 0:
-            Ben += 1
+
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
+
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
         else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     return None
